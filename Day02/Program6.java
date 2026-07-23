6bd63d11f7cbe989908b697c7a1d8ab2920afa28
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-23 23:43:49 +0530

    Swapping nums using 3rd variable

---
+++ Day02/Program6.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int num1=sc.nextInt(),num2=sc.nextInt();
+      int temp=num1;
+      num1=num2;
+      num2=temp;
+      System.out.println(num1+" "+num2);
+    }
+}
