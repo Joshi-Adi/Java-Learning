55899198d7ede8bf369e04656cc53837e59d4f68
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-23 23:32:57 +0530

    Simple Interest

---
+++ Day02/Program4.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      double principle=sc.nextDouble(),time=sc.nextDouble(),rate=sc.nextDouble();
+      System.out.println("SimpeInterest= "+(principle*time*rate/100);
+    }
+}
