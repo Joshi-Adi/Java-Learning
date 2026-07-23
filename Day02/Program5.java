df7d2742ed9237ffae1b36485a3b8b5515cd3ffc
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-23 23:38:52 +0530

    Temp converter

---
+++ Day02/Program5.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      double celsius=sc.nextDouble();
+      System.out.println("Fahrenheit= "+((celsius*9/5)+32));
+    }
+}
