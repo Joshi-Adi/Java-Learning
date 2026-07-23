b1bed1b3e2519716205a2fff8cf0bab543165667
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-23 23:26:03 +0530

    Area of rectangle

---
+++ Day02/Program3.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int length=sc.nextInt(),breadth=sc.nextInt();
+    System.out.println("AREA="+length*breadth);
+  }
+}
