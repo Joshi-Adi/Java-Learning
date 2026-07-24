f6059b8cbd82c760de66742082cec72afb592da3
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-24 22:04:12 +0530

    Relations operations

---
+++ Day 03/Program10.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println((10>5)+"\n"+(5 == 8)+"\n"+(10 != 2)+"\n"+(true && false)+"\n"+(true || false)+"\n"+(!true));
+  }
+}
