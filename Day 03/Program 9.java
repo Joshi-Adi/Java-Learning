5ad9d45778fd8ba7d77b0d484bc7b40b459e0c3b
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-24 21:57:59 +0530

    Increment and decrement

---
+++ Day 03/Program 9.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int a=10;
+    System.out.println("Post/Pre-increment");
+    System.out.println((a++)+"\n"+(a--));
+    int b=15;
+    System.out.println("Post/Pre-decrement");
+    System.out.println((++b)+"\n"+(--b));
+  }
+}
