e28c4ff4d96675cd24e46beb97ad72965c92ba73
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-24 21:53:43 +0530

    Type casting

---
+++ Day 03/Program 8.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int a=10;
+    double num=a;
+    System.out.println(num);
+    double pi=3.14;
+    int PI= (int) pi;
+    System.out.println(PI);
+  }
+}
