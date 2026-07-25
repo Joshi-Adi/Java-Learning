1046a21cadad0a407d01fb980f66d715c12afaf2
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-25 22:52:27 +0530

    Leap Year check

---
+++ Day04/Leap Year Check.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class EvenOddChecker {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);  
+       System.out.print("Enter Year\n");
+      int num=sc.nextInt();
+        if ((num % 4 == 0 && num %100 !=0)|| (num%400==0)) {
+            System.out.println(num + " is leap year");
+        } else {
+            System.out.println(num + " is not leap year");
+        }
+    }
+}
