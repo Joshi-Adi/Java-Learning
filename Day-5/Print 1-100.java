c580866f095292571a2f318cf3d3da8e02a7feaf
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-26 23:45:07 +0530

    Print numbers 1 to 100

--- Day-5/Print 1-100.java
+++ Day-5/Print 1-100.java
@@ -3,7 +3,7 @@ public class main {
     public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);  
       for(int i=1;i<=100;i++){
-            System.out.println("Hello");
+            System.out.println(i);
         }
     }
 }
