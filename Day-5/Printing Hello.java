950ba1a454d9f4a34a45ca690379f09f92b2a383
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-27 00:26:53 +0530

    Printing hello 10times

--- Day-5/Printing Hello.java
+++ Day-5/Printing Hello.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class main {
     public static void main(String[] args) {
-      Scanner sc=new Scanner(System.in);  
       for(int i=0;i<10;i++){
             System.out.println("Hello");
         }
