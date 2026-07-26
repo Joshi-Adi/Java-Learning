64ca6d01762fcebecb2bc5daff7238a3885889a2
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-27 00:02:53 +0530

    Add Reverse Counting program in Java

---
+++ Day-5/Reverse Counting.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);  
+      System.out.print("Enter number\n");
+      int n=sc.nextInt();
+      while(n>0){
+        System.out.print(n+" "); n--;}
+    }
+}
