da8895256754d78f19f6885088b4078931af6796
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-27 00:07:17 +0530

    Add Java program to count digits in a number

---
+++ Day-5/Count Digits in Num.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);  
+      System.out.print("Enter number\n");
+      int n=sc.nextInt(),count=0;
+      while(n>0){
+        int rem=n%10;
+        count+=1;n/=10;
+        }
+      System.out.println("Digits are "+count);
+    }
+}
