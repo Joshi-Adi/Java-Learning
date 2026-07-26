41dd3f05e68dfdb92f49c1d9765b097fa6b31fef
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-26 23:44:47 +0530

    printin 100-1

---
+++ Day-5/Printing 100-1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);  
+      for(int i=100;i>0;i--){
+            System.out.println(i);
+        }
+    }
+}
