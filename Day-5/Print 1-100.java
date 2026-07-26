9a50c54f076b01fb3a4e0e7c5e5377b30cbb0cf0
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-26 23:42:11 +0530

    Printing 100 numbers

---
+++ Day-5/Print 1-100.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);  
+      for(int i=1;i<=100;i++){
+            System.out.println("Hello");
+        }
+    }
+}
