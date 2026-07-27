9ecc183c232a9cf7abcb81b2bc40e189f3172639
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-28 00:40:23 +0530

    Add Java program to reverse a number

---
+++ Day 06/Reverse a Number.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Reversenumb {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number\n");
+        int n=sc.nextInt(),sum=0;
+        while (n>0){
+            int r=n%10;
+            sum=sum*10+r;
+            n/=10;
+        }
+        System.out.println("Sum of digits is "+sum);
+    }
+}
