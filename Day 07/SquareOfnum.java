d56f95a49b24315c93ba7fa3fa597a172256c249
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-28 22:05:54 +0530

    Add SquareOfnum.java to calculate square of a number

---
+++ Day 07/SquareOfnum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main{
+  static int square(int n){
+    return n*n;
+  }
+  public static void main(String[] args){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter Number\n");
+      int n=sc.nextInt();
+      System.out.println(square(n));
+  }
+}
