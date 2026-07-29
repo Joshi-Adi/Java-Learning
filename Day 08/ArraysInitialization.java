3f76c96ee96efe4c7fee593844166efec53aa417
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-29 22:47:50 +0530

    Initialize and print array in ArraysInitialization.java

---
+++ Day 08/ArraysInitialization.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5};
+        for(int i:arr){
+            System.out.print(i+" ");
+        }
+    }
+}
