aa62ddf5e8e5f11620c8e150bd5a15a65727857b
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-29 23:23:17 +0530

    Add ReversePrinting.java for reverse array output

---
+++ Day 08/ReversePrinting.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int arr[]={10,20,30,60,40,100,50,70,80,90};
+        for(int i=arr.length-1;i>=0;i--) System.out.print(arr[i]+" ");
+    }
+}
