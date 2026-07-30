8603aa2013b0fe1d887e9649203c4ff2161325de
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-30 23:54:43 +0530

    Add secondLargest.java to find second largest number
    Implement logic to find the second largest number in an array.

---
+++ Day 09/secondLargest.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        int arr[]={10,20,30,5,60};
+        int first=arr[0],second=arr[0];
+        for(int i:arr){
+            if (i>first){ second=first;first=i;}
+            else if(i>second && i!=first) second=i;
+        }
+        System.out.println("Freq of give num is: "+second);
+    }
+}
