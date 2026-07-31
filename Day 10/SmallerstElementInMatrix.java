d3e7d7a642a1b805dd4cdae708a7ff49431b4320
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-31 23:31:01 +0530

    Add program to find smallest element in matrix
    Implement a Java program to find the smallest element in a 3x3 matrix.

---
+++ Day 10/SmallerstElementInMatrix.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        int min=arr[0][0];
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(arr[i][j]<min) min=arr[i][j];
+            }
+        }
+        System.out.println("Smallest Element in matrix is "+min);
+    }
+}
