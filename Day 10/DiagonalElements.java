cf046c2ba2eef43c60b17bb6587cb962ce71a5cd
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-07-31 23:38:53 +0530

    Add diagonal elements printing for a 3x3 matrix
    Prints the diagonal elements of a 3x3 matrix.

---
+++ Day 10/DiagonalElements.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for(int i=0;i<3;i++){
+                System.out.println(arr[i][i]);
+            }
+    }
+}
