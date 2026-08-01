7c9b5b8932a395d0d9d9f9d0a3f537edb62288a6
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-02 00:34:46 +0530

    Add PrintingEachChar.java to print each character

---
+++ Day 11/PrintingEachChar.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string1="BEAUTIFUL";
+        for(int i=0;i<string1.length();i++){
+            char ch=string1.charAt(i);
+            System.out.println(ch);
+        }
+        }
+}
