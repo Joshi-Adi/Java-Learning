dd8f861423c4a0dcafcbd289634a7815e6f47884
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-02 00:27:51 +0530

    Add ConvertToLowerCase.java to convert string input

---
+++ Day 11/ConvertToLowerCase.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        System.out.println("Name is "+name);
+        System.out.println("In uppercases "+name.toLowerCase());
+    }
+}
