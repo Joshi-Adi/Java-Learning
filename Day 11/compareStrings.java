66b277187d2f690bfee2a0ad4715689daeed5388
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-02 00:30:27 +0530

    Add compareStrings.java to compare two strings

---
+++ Day 11/compareStrings.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string1=sc.nextLine(),string2=sc.nextLine();
+        System.out.println(string1.equals(string2)?"Both are same":"Both are not same");}
+}
