fec4e60c19a84bffa4b05d21d5d042e66b1217ee
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-02 00:42:12 +0530

    Add CountVowels.java to count vowels in a string

---
+++ Day 11/CountVowels.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string1=sc.nextLine().toLowerCase();
+        int count=0;
+        for(int i=0;i<string1.length();i++){
+            char ch=string1.charAt(i);
+             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') count++;
+        }
+        System.out.println("Total vowels are "+count);
+        }
+}import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string1=sc.nextLine().toLowerCase();
+        int count=0;
+        for(int i=0;i<string1.length();i++){
+            char ch=string1.charAt(i);
+             if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') count++;
+        }
+        System.out.println("Total vowels are "+count);
+        }
+}
