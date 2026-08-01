6f4788c108f35dd58e73921b18aa797ffa23a61e
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-02 00:31:52 +0530

    Add CompareIgnoreCase.java for case insensitive comparison

---
+++ Day 11/CompareIgnoreCase.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String string1=sc.nextLine(),string2=sc.nextLine();
+        System.out.println(string1.equalsIgnoreCase(string2)?"Both are same":"Both are not same");}
+}
