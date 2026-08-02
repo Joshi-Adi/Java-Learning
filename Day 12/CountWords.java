34c440326f8dc58a6991657323b7d597617caa53
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-03 00:44:50 +0530

    Update CountWords.java

--- Day 12/CountWords.java
+++ Day 12/CountWords.java
@@ -2,7 +2,7 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter string to check palindrome\n");
+        System.out.print("Enter sentence to find word count\n");
         String sent=sc.nextLine();
         int count=0;
         String[] words=sent.split(" ");
