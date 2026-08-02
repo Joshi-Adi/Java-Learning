b19ad3ab0e2c542393d421750c9b39a4e8bac328
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-03 00:42:34 +0530

    Add CountWords.java to count words in input string
    Implement a program to count words in a string.

---
+++ Day 12/CountWords.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter string to check palindrome\n");
+        String sent=sc.nextLine();
+        int count=0;
+        String[] words=sent.split(" ");
+        System.out.println(words.length);
+    }
+}
