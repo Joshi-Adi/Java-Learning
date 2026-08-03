f1621a8bc7822110586b8348eae23678b754c67e
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-04 00:56:12 +0530

    Add Student class and main method for output

---
+++ Day 13/Class&Obj.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Student{
+    String name;int age;
+}
+class Main {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.name="Raju";s.age=24;
+        System.out.println(s.name+" "+s.age);
+    }
+}
