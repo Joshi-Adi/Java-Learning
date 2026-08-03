7a470ad79de9835206486261cda27e80125b9914
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-04 01:01:15 +0530

    Add Employee class with main method for testing

---
+++ Day 13/Employee.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Employee{
+    int id;int salary;
+}
+class Main {
+    public static void main(String[] args) {
+        Employee e=new Employee();
+        e.id=221;e.salary=11000;
+        System.out.print(e.id+" "+e.salary);
+    }
+}
