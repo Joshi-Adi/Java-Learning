44034f1d93baedcc05c5388b472036486a5d12b2
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-04 22:59:51 +0530

    Add Circle class with area calculation

---
+++ Day 14/Circle.java
@@ -0,0 +1,15 @@
+class Circle{
+    int radius;
+    Circle(int r){
+        radius=r;
+    }
+    void area(){
+        System.out.println("Area "+ Math.PI*radius*radius);
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Circle c=new Circle(5);
+        c.area();
+    }
+}
