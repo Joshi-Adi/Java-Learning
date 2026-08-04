39ea3b33b5ae2e9a229c2f0125df2766eecc1f2e
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-04 22:43:17 +0530

    Add Student class with constructor and main method

---
+++ Day 14/Student.java
@@ -0,0 +1,10 @@
+class Student{
+    Student(){
+        System.out.println("Student Created");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Student s=new Student();
+    }
+}
