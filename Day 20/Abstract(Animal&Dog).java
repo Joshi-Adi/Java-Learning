23b9eece1b384b9391b0a86b158a7d1d5b29261a
Joshi <66480820+Joshi-Adi@users.noreply.github.com>
2026-08-10 23:55:57 +0530

    Add abstract Animal class and Dog implementation

---
+++ Day 20/Abstract(Animal&Dog).java
@@ -0,0 +1,15 @@
+abstract class Animal{
+    abstract void sound();
+}
+class dog extends Animal{
+    @Override
+    public void sound(){
+        System.out.println("Dog Barks");
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        Animal a=new dog();
+        a.sound();
+    }
+}
